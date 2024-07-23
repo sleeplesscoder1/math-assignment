0fb4ab774a8cb514b2873c7619afd53805aebc74
sleeplesscoder1 <nivya230906@gmail.com>
2024-07-23 23:32:26 +0530

    Update question3.m

--- question3.m
+++ question3.m
@@ -3,26 +3,28 @@ t = 1;
 c = 1;
 dx = 0.1;
 dt = 0.1;
+t= 0:dt:1;
 k = (c * dt/dx)^2;
+
 for i=1:11
     x(i) = (i-1)*dx;
 end
 
 for i=1:11
-    u(i) =2* x(i)*(1-x(i));
+    u(i,1) =2* x(i)*(1-x(i));
 end
+u(:,2) = u(:,1);
 
 for j=2:10
     for i=2:10
-        u1(i) = (k * (u(i+1)-2*u(i)+u(i-1))) + 2*u(i) - u(j-1);
+        u1(i,j+1) = k * (u(i+1,j)-2*u(i,j)+u(i-1,j))+ 2*u(i,j) - u(i,j-1);
     end
-    u1(1) = 0;
-    u1(11) = 0;
-    u = u1;
+    u1(1,:) = 0;
+    u1(11,:) = 0;
+    u= u1;
 end
-plot(x,u1)
-xlabel("x")
-ylabel("t")
-title("displacement vs time graph at t=1s")
+
+plot(t, u1)
+
 
  
